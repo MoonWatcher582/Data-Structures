6e5b938ea35c985d41b697b6a9793dbff8671914
MoonWatcher582 <erbronne@gmail.com>
2014-07-19 20:26:47 -0400

    Functions commented, add/remove_edge functions added to vertex class, __eq__ functions added to all classes

--- graph.py
+++ graph.py
@@ -7,7 +7,15 @@ class Vector(object):
 		self.magnitude = mag
 	
 	def __str__(self):
-		return 'Degrees: ' + str(self.degrees) + '\nMagnitude: ' + str(self.magnitude)		
+		return 'Degrees: ' + str(self.degrees) + '\nMagnitude: ' + str(self.magnitude)	
+
+	def __eq__(self,other):
+		#two vectors are equal if they have the same degree and magnitude
+		if other is None:
+			return False
+		if self.degrees == other.degrees and self.magnitude == other.magnitude:
+			return True
+		return False
 
 class Edge(object):
 
@@ -19,6 +27,14 @@ class Edge(object):
 
 	def __str__(self):
 		return 'Vector: \n' + str(self.vector) + '\nVertex Coordinates: (' + str(self.vertex.x) + ',' + str(self.vertex.y) + ',' + str(self.vertex.z) + ')'
+
+	def __eq__(self,other):
+		#two edges are equal if they have the same vector and vertex reference
+		if other is None:
+			return False
+		if self.vector == other.vector and self.vertex == other.vertex:
+			return True
+		return False
 
 class Vertex(object):
 
@@ -32,11 +48,34 @@ class Vertex(object):
 		self.confidence = conf
 		self.edge_list = []
 
+	def __eq__(self,other):
+		#two verticies are equal if they are at the same x,y,z coordinate, I assume no further checking is required
+		if other is None:
+			return False
+		if self.x == other.x and self.y == other.y and self.z == other.z:
+			return True
+		return False
+
 	def set_as_room(self):
+		#set the node as a room, rather than a decision
 		self.isRoom = True
 
+	def unset_as_room(self):
+		#not sure if this has any purpose, but here it is
+		self.isRoom = False
+
 	def add_edge(self, edge):
+		#add this edge to the list, and add this vertex to the edge's vertex's list
 		self.edge_list.append(edge)
+		to_add = Edge(edge.vector, self)
+		edge.vertex.edge_list.append(to_add)
+
+	def remove_edge(self, edge):
+		#remove this edge from the list, and remove this vertex from the edge's vertex's list
+		reciprocate = edge.vertex
+		self.edge_list.remove(edge)
+		other_edge = Edge(edge.vector, self)
+		reciprocate.edge_list.remove(other_edge)	
 
 	def __str__(self):
 		coord = 'Coordinates: (' + str(self.x) + ',' + str(self.y) + ',' + str(self.z) +')'
@@ -57,9 +96,16 @@ class Graph(object):
 		self.vertex_list = []
 
 	def add_vertex(self, vertex):
+		#add a new vertex to the list
 		self.vertex_list.append(vertex)
 
+	def remove_vertex(self, vertex):
+		#remove this vertex from the list
+		self.vertex_list.remove(vertex)
+
 	def form_edge(self,vertex_one,vertex_two,vector):
+		#create an edge between these two verticies
+		#essentially an alternative to the vertex's built-in add_edge function because I forgot I wrote this when I wrote them
 		vertex_one.add_edge(Edge(vector,vertex_two))
 		vertex_two.add_edge(Edge(vector,vertex_one))
 
